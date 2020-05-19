36f99cd41cf684d2d8a6398e796e28b5341253df
lhj <skybluehj930@gmail.com>
2020-05-19 14:15:12 +0900

    BoardDAO 5/18

--- src/com/board/BoardDAO.java
+++ src/com/board/BoardDAO.java
@@ -2,6 +2,9 @@ package com.board;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.util.DBConn;
 
@@ -32,4 +35,199 @@ public class BoardDAO {
 			}
 		}
 	}
+	
+	// 전체 데이터 갯수
+		public int dataCount() {
+			int result = 0;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			String sql;
+			
+			try {
+				sql = "SELECT NVL(COUNT(*),0) FROM board";
+				pstmt = conn.prepareStatement(sql);
+				
+				rs = pstmt.executeQuery();
+				if(rs.next()) {
+					result = rs.getInt(1);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				if(rs != null) {
+					try {
+						rs.close();
+					} catch (Exception e2) {
+						e2.printStackTrace();
+					}
+				}
+				if(pstmt != null) {
+					try {
+						rs.close();
+					} catch (Exception e2) {
+						e2.printStackTrace();
+					}
+				}
+			}	
+			return result;
+		}
+		
+		// 검색했을때 데이터갯수
+		public int dataCount(String condition, String keyword) {
+			int result = 0;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			StringBuilder sb = new StringBuilder();
+			
+			try {
+				sb.append("SELECT NVL(COUNT(*),0) FROM board b ");
+				sb.append("JOIN member m ON m.uerId = b.id ");
+				if(condition.equals("write")) {
+					sb.append("WHERE INSTR(name, ? ) = 1");
+				}else if(condition.equals("contents")) {
+					sb.append("WHERE INSTR(content,?) >=1");
+				}else {
+					sb.append("WHERE INSTR(" + condition + ", ?) >= 1");
+				}
+				
+				pstmt = conn.prepareStatement(sb.toString());
+				pstmt.setString(1, keyword);
+				
+				rs = pstmt.executeQuery();
+				if(rs.next()) {
+					result = rs.getInt(1);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				if(rs != null) {
+					try {
+						rs.close();
+					} catch (Exception e2) {
+						e2.printStackTrace();
+					}
+				}
+				
+				if(pstmt != null) {
+					try {
+						pstmt.close();
+					} catch (Exception e2) {
+						e2.printStackTrace();
+					}
+				}
+			}
+			return result;
+		}
+		
+		public List<BoardDTO> allBoard(int offset, int rows){
+			List<BoardDTO> list = new ArrayList<BoardDTO>();
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			StringBuilder sb = new StringBuilder();
+			
+			try {
+				sb.append("SELECT boardNum, title,name, TO_CHAR(created,'YYYY-MM-DD') created, viewCount ");
+				sb.append("FROM board ");
+				sb.append("ORDER BY boardNum DESC ");
+				sb.append("OFFSET ? ROWS FETCH FIRST ? ROWS ONLY ");
+				
+				pstmt = conn.prepareStatement(sb.toString());
+				pstmt.setInt(1, offset);
+				pstmt.setInt(2, rows);
+				
+				rs = pstmt.executeQuery();
+				while(rs.next()) {
+					BoardDTO dto = new BoardDTO();
+					
+					dto.setNum(rs.getInt("boardNum"));
+					dto.setName(rs.getString("name"));
+					dto.setTitle(rs.getString("title"));
+					dto.setCreated(rs.getString("created"));
+					dto.setViewCount(rs.getInt("viewCount"));
+					
+					list.add(dto);
+				}
+			
+			} catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				if(rs != null) {
+					try {
+						rs.close();
+					} catch (Exception e2) {
+						e2.printStackTrace();
+					}
+				}
+				
+				if(pstmt != null) {
+					try {
+						pstmt.close();
+					} catch (Exception e2) {
+						e2.printStackTrace();
+					}
+				}
+			}
+			
+			return list;
+		}
+		
+		public List<BoardDTO> allBoard(int offset, int rows, String condition, String keyword){
+			List<BoardDTO> list = new ArrayList<BoardDTO>();
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			StringBuilder sb = new StringBuilder();
+			
+			try {
+				sb.append("SELECT boardNum, title,name, TO_CHAR(created,'YYYY-MM-DD') created, viewCount ");
+				sb.append("FROM board ");
+				if(condition.equals("write")) {
+					sb.append("WHERE INSTR(name, ? ) = 1");
+				}else if(condition.equals("contents")) {
+					sb.append("WHERE INSTR(content,?) >=1");
+				}else {
+					sb.append("WHERE INSTR(" + condition + ", ?) >= 1");
+				}
+				sb.append("ORDER BY boardNum DESC ");
+				sb.append("OFFSET ? ROWS FETCH FIRST ? ROWS ONLY ");
+				
+				pstmt = conn.prepareStatement(sb.toString());
+				pstmt.setString(1, keyword);
+				pstmt.setInt(2, offset);
+				pstmt.setInt(3, rows);
+				
+				rs = pstmt.executeQuery();
+				while(rs.next()) {
+					BoardDTO dto = new BoardDTO();
+					
+					dto.setNum(rs.getInt("boardNum"));
+					dto.setName(rs.getString("name"));
+					dto.setTitle(rs.getString("title"));
+					dto.setCreated(rs.getString("created"));
+					dto.setViewCount(rs.getInt("viewCount"));
+					
+					list.add(dto);
+				}
+			
+			} catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				if(rs != null) {
+					try {
+						rs.close();
+					} catch (Exception e2) {
+						e2.printStackTrace();
+					}
+				}
+				
+				if(pstmt != null) {
+					try {
+						pstmt.close();
+					} catch (Exception e2) {
+						e2.printStackTrace();
+					}
+				}
+			}
+			
+			return list;
+		}
 }
