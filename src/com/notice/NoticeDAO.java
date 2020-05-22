ee379391f6404a7d02e058582ec23976df6466be
SIST <SIST@DESKTOP-U7G5SA7>
2020-05-22 17:18:12 +0900

    김종완

---
+++ src/com/notice/NoticeDAO.java
@@ -0,0 +1,585 @@
+package com.notice;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.util.DBConn;
+
+public class NoticeDAO {
+	private Connection conn = DBConn.getConnection();
+
+	public void insertNotice(NoticeDTO dto) {
+		StringBuilder sb = new StringBuilder();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+		int seq;
+		try {
+
+			sql = "SELECT notice_seq.NEXTVAL FROM dual";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			seq = 0;
+			if (rs.next()) {
+				seq = rs.getInt(1);
+			}
+			dto.setNum(seq);
+			rs.close();
+			pstmt.close();
+			rs = null;
+			pstmt = null;
+
+			sb.append("INSERT INTO notice");
+			sb.append("(num,id,name,title,content,important) ");
+			sb.append("VALUES(?,?,?,?,?,?)");
+
+			pstmt = conn.prepareStatement(sb.toString());
+			pstmt.setInt(1, dto.getNum());
+			pstmt.setString(2, dto.getId());
+			pstmt.setString(3, dto.getName());
+			pstmt.setString(4, dto.getTitle());
+			pstmt.setString(5, dto.getContent());
+			pstmt.setInt(6, dto.getImportant());
+
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+	}
+
+	public void insertNoticeFile(NoticeDTO dto) {
+		StringBuilder sb = new StringBuilder();
+		PreparedStatement pstmt = null;
+
+		try {
+			sb.append("INSERT INTO noticeFile");
+			sb.append("(filenum, num,saveFilename,originalFilename,filesize) ");
+			sb.append("VALUES(noticefile_seq.NEXTVAL,?,?,?,?)");
+
+			pstmt = conn.prepareStatement(sb.toString());
+			pstmt.setInt(1, dto.getNum());
+			pstmt.setString(2, dto.getSaveFileName());
+			pstmt.setString(3, dto.getOriginalFileName());
+			pstmt.setLong(4, dto.getFileSize());
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+	}
+
+	public int dataCount() {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+
+		try {
+			sql = "SELECT NVL(COUNT(*), 0) FROM notice";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			if (rs.next())
+				result = rs.getInt(1);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+		return result;
+	}
+
+	public int dataCount(String condition, String keyword) {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		StringBuilder sb = new StringBuilder();
+
+		try {
+			sb.append("SELECT NVL(COUNT(*),0) FROM board ");
+			if (condition.equals("writer")) {
+				sb.append("WHERE INSTR(name, ? ) = 1");
+			} else if (condition.equals("contents")) {
+				sb.append("WHERE INSTR(content,?) >=1");
+			} else {
+				sb.append("WHERE INSTR(" + condition + ", ?) >= 1");
+			}
+
+			pstmt = conn.prepareStatement(sb.toString());
+			pstmt.setString(1, keyword);
+
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				result = rs.getInt(1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+					e2.printStackTrace();
+				}
+			}
+
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+					e2.printStackTrace();
+				}
+			}
+		}
+		return result;
+	}
+
+	public List<NoticeDTO> listNotice(int offset, int rows) {
+		List<NoticeDTO> list = new ArrayList<NoticeDTO>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		StringBuilder sb = new StringBuilder();
+
+		try {
+			sb.append("SELECT num, id, name, title, ");
+			sb.append("       hitCount, TO_CHAR(created,'YYYY-MM-DD') created ");
+			sb.append(" FROM notice ");
+			sb.append(" ORDER BY num DESC  ");
+			sb.append(" OFFSET ? ROWS FETCH FIRST ? ROWS ONLY");
+
+			pstmt = conn.prepareStatement(sb.toString());
+			pstmt.setInt(1, offset);
+			pstmt.setInt(2, rows);
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				NoticeDTO dto = new NoticeDTO();
+
+				dto.setNum(rs.getInt("num"));
+				dto.setId(rs.getString("id"));
+				dto.setName(rs.getString("name"));
+				dto.setTitle(rs.getString("title"));
+				dto.setViewCount(rs.getInt("hitCount"));
+				dto.setCreated(rs.getString("created"));
+
+				list.add(dto);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+		return list;
+	}
+
+	public List<NoticeDTO> listNotice(int offset, int rows, String condition, String keyword) {
+		List<NoticeDTO> list = new ArrayList<NoticeDTO>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		StringBuilder sb = new StringBuilder();
+
+		try {
+			sb.append("SELECT num, id, name, title, ");
+			sb.append("       hitCount, TO_CHAR(created,'YYYY-MM-DD') created ");
+			sb.append(" FROM notice ");
+			if (condition.equals("write")) {
+				sb.append("WHERE INSTR(name, ? ) = 1");
+			} else if (condition.equals("contents")) {
+				sb.append("WHERE INSTR(content,?) >=1");
+			} else {
+				sb.append("WHERE INSTR(" + condition + ", ?) >= 1");
+			}
+			sb.append(" ORDER BY num DESC  ");
+			sb.append(" OFFSET ? ROWS FETCH FIRST ? ROWS ONLY");
+
+			pstmt = conn.prepareStatement(sb.toString());
+			pstmt.setString(1, keyword);
+			pstmt.setInt(2, offset);
+			pstmt.setInt(3, rows);
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				NoticeDTO dto = new NoticeDTO();
+
+				dto.setNum(rs.getInt("num"));
+				dto.setId(rs.getString("id"));
+				dto.setName(rs.getString("name"));
+				dto.setTitle(rs.getString("title"));
+				dto.setViewCount(rs.getInt("hitCount"));
+				dto.setCreated(rs.getString("created"));
+
+				list.add(dto);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+		return list;
+	}
+
+	public NoticeDTO readNotice(int num) {
+		NoticeDTO dto = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		StringBuilder sb = new StringBuilder();
+		try {
+			sb.append("SELECT num,important,id,name,title,content,hitCount,created ");
+			sb.append("FROM notice ");
+			sb.append("WHERE num = ?");
+
+			pstmt = conn.prepareStatement(sb.toString());
+			pstmt.setInt(1, num);
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				dto = new NoticeDTO();
+
+				dto.setNum(rs.getInt("num"));
+				dto.setImportant(rs.getInt("important"));
+				dto.setId(rs.getString("id"));
+				dto.setName(rs.getString("name"));
+				dto.setTitle(rs.getString("title"));
+				dto.setContent(rs.getString("content"));
+				dto.setViewCount(rs.getInt("hitCount"));
+				dto.setCreated(rs.getString("created"));
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+		return dto;
+	}
+
+	public List<NoticeDTO> fileList(int num) {
+		List<NoticeDTO> list = new ArrayList<NoticeDTO>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		StringBuilder sb = new StringBuilder();
+		try {
+			sb.append("SELECT fileNum, num, saveFileName, originalFileName, fileSize ");
+			sb.append("FROM noticeFile ");
+			sb.append("WHERE num = ?");
+
+			pstmt = conn.prepareStatement(sb.toString());
+			pstmt.setInt(1, num);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				NoticeDTO dto = new NoticeDTO();
+
+				dto.setFileNum(rs.getInt("fileNum"));
+				dto.setNum(rs.getInt("num"));
+				dto.setSaveFileName(rs.getString("saveFileName"));
+				dto.setOriginalFileName(rs.getString("originalFileName"));
+				dto.setFileSize(rs.getLong("fileSize"));
+
+				list.add(dto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+		return list;
+	}
+
+	public int updateHitCount(int num) {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		String sql;
+
+		try {
+			sql = "UPDATE notice SET hitCount=hitCount+1 WHERE num=?";
+
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			result = pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+		return result;
+	}
+
+	public List<NoticeDTO> importantList() {
+		List<NoticeDTO> list = new ArrayList<NoticeDTO>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		StringBuilder sb = new StringBuilder();
+
+		try {
+			sb.append("SELECT num,id,name,title,hitCount,created ");
+			sb.append("FROM notice ");
+			sb.append("WHERE important = 1 ");
+			sb.append(" ORDER BY created DESC ,num DESC  ");
+
+			pstmt = conn.prepareStatement(sb.toString());
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				NoticeDTO dto = new NoticeDTO();
+
+				dto.setNum(rs.getInt("num"));
+				dto.setId(rs.getString("id"));
+				dto.setName(rs.getString("name"));
+				dto.setTitle(rs.getString("title"));
+				dto.setCreated(rs.getString("created"));
+				dto.setViewCount(rs.getInt("hitCount"));
+
+				list.add(dto);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+		return list;
+
+	}
+
+	public int updateNotice(NoticeDTO dto) {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		String sql;
+
+		try {
+			sql = "UPDATE notice SET important=?, title=?, content=?, created = sysdate";
+			sql += " WHERE num=?";
+
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, dto.getImportant());
+			pstmt.setString(2, dto.getTitle());
+			pstmt.setString(3, dto.getContent());
+			pstmt.setInt(4, dto.getNum());
+			result = pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+		return result;
+	}
+
+	public int deleteNotice(int num) {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		String sql;
+
+		try {
+			sql = "DELETE FROM noticeFile WHERE num = ? ";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			result = pstmt.executeUpdate();
+			pstmt.close();
+			
+			
+			sql = "DELETE FROM notice WHERE num = ? ";
+
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			result = pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+		return result;
+	}
+
+	public NoticeDTO readFileNum(int fileNum) {
+		NoticeDTO dto = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		StringBuilder sb = new StringBuilder();
+		try {
+			sb.append("SELECT fileNum,num,saveFileName, originalFileName, fileSize ");
+			sb.append("FROM noticefile ");
+			sb.append("WHERE fileNum = ?");
+
+			pstmt = conn.prepareStatement(sb.toString());
+			pstmt.setInt(1, fileNum);
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				dto = new NoticeDTO();
+
+				dto.setFileNum(rs.getInt("fileNum"));
+				dto.setNum(rs.getInt("num"));
+				dto.setSaveFileName(rs.getString("saveFileName"));
+				dto.setOriginalFileName(rs.getString("originalFileName"));
+				dto.setFileSize(rs.getLong("fileSize"));
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+		return dto;
+	}
+	
+	public int deleteNoticeFile(int fileNum) {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		String sql;
+
+		try {
+			sql = "DELETE FROM noticeFile WHERE fileNum = ? ";
+
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, fileNum);
+			result = pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+		return result;
+	}
+	
+}
